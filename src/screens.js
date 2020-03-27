6bf404f21d45f3048bec48ecdcb51992695b4b0f
Spring Raindrop <SpringRaindrop@vivaldi.net>
2020-03-27 20:06:50 +0000

    Add smaller text size

--- src/screens.js
+++ src/screens.js
@@ -17,3 +17,17 @@ export function getSmallTextSize(width) {
 
   return 16;
 }
+
+export function getSmallerTextSize(width) {
+  if (width >= 520) {
+    return 15;
+  } else if (width >= 450) {
+    return 14;
+  } else if (width >= 420) {
+    return 12;
+  } else if (width >= 320) {
+    return 10;
+  }
+
+  return 6;
+}
