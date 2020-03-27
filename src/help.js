c4d935b9f10f3b7dd8082c82b13b1e2cd5b54288
Spring Raindrop <SpringRaindrop@vivaldi.net>
2020-03-27 20:03:00 +0000

    Make help text size configurable and background darker; remove spaces

--- src/help.js
+++ src/help.js
@@ -1,6 +1,4 @@
-import { getSmallTextSize } from "./screens.js";
-
-export function showHelp(canvasEl, ctx, width, height, text) {
+export function showHelp(canvasEl, ctx, width, height, text, size) {
   let _resolve;
 
   const clickHandler = e => {
@@ -10,12 +8,11 @@ export function showHelp(canvasEl, ctx, width, height, text) {
 
   ctx.save();
 
-  const padding = width / 20;
-  ctx.fillStyle = "#000000aa";
+  const padding = width / 30;
+  ctx.fillStyle = "#000000cc";
   ctx.fillRect(padding, padding, width - 2 * padding, height - 2 * padding);
 
   const lines = text.split("\n");
-  const size = getSmallTextSize(width);
   ctx.textBaseline = "top";
   ctx.textAlign = "left";
   ctx.fillStyle = "white";
@@ -25,6 +22,9 @@ export function showHelp(canvasEl, ctx, width, height, text) {
   for (let line of lines) {
     let fits = line.length;
     while (line) {
+      if (line[0] === " ")
+        line = line.slice(1);
+
       if (ctx.measureText(line.slice(0, fits)).width <= width - padding * 4) {
         ctx.fillText(line.slice(0, fits), padding * 2, padding * 2 + size * 1.2 * n);
         line = line.slice(fits);
