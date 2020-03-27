3cfab3ead575f23fcab29b92d607a8c5885c33d7
Spring Raindrop <SpringRaindrop@vivaldi.net>
2020-03-27 17:10:13 +0000

    Add menu mouse handlers and hover styles

--- src/menu.js
+++ src/menu.js
@@ -10,6 +10,8 @@ class Menu {
     this._beforeRedraw = null;
 
     this._renderedOptions = [];
+    this._selectedOption = -1;
+    this._listeners = [];
   }
 
   addOption(text, callback) {
@@ -72,8 +74,10 @@ class Menu {
     this.ctx.textBaseline = "middle";
     this.ctx.textAlign = "center";
 
-    for (const opt of this._renderedOptions) {
-      this.ctx.fillStyle = "black";
+    for (let i = 0; i < this._renderedOptions.length; i++) {
+      const opt = this._renderedOptions[i];
+
+      this.ctx.fillStyle = i !== this._selectedOption ? "black" : "#333";
       this.ctx.fillRect(opt.button.x, opt.button.y, opt.button.width, opt.button.height);
 
       this.ctx.fillStyle = "white";
@@ -86,6 +90,47 @@ class Menu {
 
   set onBeforeRedraw(fn) {
     this._beforeRedraw = fn;
+  }
+
+  _clickHandler(e) {
+    if (this._selectedOption !== -1)
+      this._renderedOptions[this._selectedOption].callback();
+  }
+
+  _mousemoveHandler(e) {
+    let selected = -1;
+    for (let i = 0; i < this._renderedOptions.length; i++) {
+      const opt = this._renderedOptions[i];
+      if (e.offsetX >= opt.button.x &&
+        e.offsetX <= opt.button.x + opt.button.width &&
+        e.offsetY >= opt.button.y &&
+        e.offsetY <= opt.button.y + opt.button.height) {
+        selected = i;
+        break;
+      }
+    }
+
+    if (this._selectedOption !== selected) {
+      this._selectedOption = selected;
+      this.draw.call(this);
+    }
+  }
+
+  installHandlers(canvasEl) {
+    const onclick = { on: "click", fn: this._clickHandler.bind(this) };
+    const onmousemove = { on: "mousemove", fn: this._mousemoveHandler.bind(this) };
+
+    this._listeners.push(onclick);
+    this._listeners.push(onmousemove);
+
+    canvasEl.addEventListener("click", onclick.fn);
+    canvasEl.addEventListener("mousemove", onmousemove.fn);
+  }
+
+  removeHandlers(canvasEl) {
+    for (const l of this.listeners) {
+      canvasEl.removeEventListener(l.on, l.fn);
+    }
   }
 }
 
