3a58b950a286a6e34e98b49e4e628f5b5eeaa18f
Spring Raindrop <SpringRaindrop@vivaldi.net>
2020-03-27 21:07:56 +0000

    Add rules and credits texts

---
+++ src/texts.js
@@ -0,0 +1,13 @@
+export const rulesText = `Rules:
+ 
+Each player is represented by a color (blue or teal). The goal of the game is to get four connected pieces of your color into any row, column or diagonal. The players move in turns. Each turn the player can place one of their pieces into any of the seven columns where each piece will fall to the lowest possible free place i.e. it will fall down until it reaches the ground level or another piece.
+ 
+After one player's move is done the second player can make their input. This is repeated until the game is over, which is when one of the players has four pieces in a row, column or diagonal or no more moves are possible because the board is filled.
+`;
+
+export const creditsText = `Credits:
+ 
+This game was created by Spring Raindrop. You can find the sources at https://github.com/cutiful/four-in-line.
+ 
+Rules were copied from KFourInLine handbook.
+`;
