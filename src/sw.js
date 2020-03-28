06fa2a89e19b300df20adecee8bedc99563d19cd
Spring Raindrop <SpringRaindrop@vivaldi.net>
2020-03-28 16:16:48 +0000

    Add revision null to s.js file

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,6 @@
 import { precacheAndRoute } from "workbox-precaching";
 
 precacheAndRoute(self.__WB_MANIFEST);
-precacheAndRoute(["https://unpkg.com/systemjs@2.0.0/dist/s.min.js"]);
+precacheAndRoute([
+  { url: "https://unpkg.com/systemjs@2.0.0/dist/s.min.js", revision: null }
+]);
